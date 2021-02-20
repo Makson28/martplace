153ad18aa3cca363882cadbbf302918c0871d92b
Makson28 <63288735+Makson28@users.noreply.github.com>
2021-02-20 22:00:24 +0200

    bug fix added new content

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,15 @@
 $(function(){
-  
+  $('.fetured__slider').slick({
+    slidesToShow: 1,
+    prevArrow: $('.left_v1'),
+    nextArrow: $('.right_v1'),
+  });
+
+  var mixer = mixitup('.product__inner');
+
+  $('.followers__slider').slick({
+    slidesToShow: 3,
+    prevArrow: $('.left_v2'),
+    nextArrow: $('.right_v2'),
+  });
 });

ef14be870f4be13b34821c681a5f3d8dc1683b34
Makson28 <63288735+Makson28@users.noreply.github.com>
2021-03-27 20:48:22 +0200

    category-page, new content, bug fix

--- app/js/main.js
+++ app/js/main.js
@@ -13,3 +13,39 @@ $(function(){
     nextArrow: $('.right_v2'),
   });
 });
+
+document.addEventListener('click', e => {
+  if (e.target.dataset.type === 'arrow') {
+    const target = e.target
+    const targetId = target.getAttribute('id')
+  
+    const filterName = targetId.split('-')[1]
+    const filter = document.getElementById(`filter-${filterName}`)
+    filter.classList.toggle('active')
+    target.classList.toggle('active')
+  }
+})
+
+$(".range__line").ionRangeSlider({
+  type: "double",
+  min: 0,
+  max: 330,
+  from: 30,
+  to: 300,
+  prefix: "$",
+  skin: "round"
+});
+
+$('.icon-th').on('click', function(){
+  $('.icon-th').addClass('active');
+  $('.icon-th-list').removeClass('active');
+  $('.category-product__list').addClass('category-product__grid');
+  $('.category-product__list').removeClass('category-product__list');
+});
+
+$('.icon-th-list').on('click', function(){
+  $('.icon-th-list').addClass('active');
+  $('.icon-th').removeClass('active');
+  $('.category-product__grid').addClass('category-product__list');
+  $('.category-product__grid').removeClass('category-product__grid');
+});
